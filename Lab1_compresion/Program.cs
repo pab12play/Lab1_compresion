ac14021eb259a00dbbfcbf83032eb6958823679e
Pablo Pineda <pablo12.izq@gmail.com>
2017-09-04 16:25:06 -0600

    Implementado, algoritmo de compresion RLE con pares de bytes

--- Lab1_compresion/Program.cs
+++ Lab1_compresion/Program.cs
@@ -11,7 +11,7 @@ namespace Lab1_compresion
     {
         static void Main(string[] args)
         {
-            //args = new string[] { "-d","-f","test.txt" };
+            args = new string[] { "-d","-f","test.comp" };
             if (args.Length == 1 && args[0].ToLower().Equals("help") )
             {
                 Console.WriteLine("Uso: .\\Lab1_compresion.exe [-d] [-c] [-f <archivo>]");
@@ -60,6 +60,36 @@ namespace Lab1_compresion
             try
             {
                 byte[] bytes = System.IO.File.ReadAllBytes(archivo);
+                List<byte> RLE = new List<byte>();
+                byte anterior=bytes[0];
+                int contador=1;
+                for (int i = 1; i < bytes.Length; i++)
+                {
+                    if (bytes[i] == anterior)
+                    {
+                        if (contador >= 256)
+                        {
+                            RLE.Add(anterior);
+                            RLE.Add(Convert.ToByte(contador));
+                            contador = 1;
+                            anterior = bytes[i];
+                        }
+                        else
+                        {
+                            contador++;
+                        }
+                    }
+                    else
+                    {
+                        RLE.Add(anterior);
+                        RLE.Add(Convert.ToByte(contador));
+                        contador = 1;
+                        anterior = bytes[i];
+                    }
+                }
+                RLE.Add(anterior);
+                RLE.Add(Convert.ToByte(contador));
+                System.IO.File.WriteAllBytes("test.comp", RLE.ToArray());
             }
             catch (FileNotFoundException e)
             {
@@ -72,6 +102,17 @@ namespace Lab1_compresion
             try
             {
                 byte[] bytes = System.IO.File.ReadAllBytes(archivo);
+                List<byte> RLE = new List<byte>();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    byte caracter = bytes[i];
+                    int contador = Convert.ToInt32(bytes[++i]);
+                    for(int j = 0; j < contador; j++)
+                    {
+                        RLE.Add(caracter);
+                    }
+                }
+                System.IO.File.WriteAllBytes("test.txt", RLE.ToArray());
             }
             catch (FileNotFoundException e)
             {
