79a85968bb88b016f4a5b9c84179c902bf71c57b
Pablo Pineda <pablo12.izq@gmail.com>
2017-09-15 07:07:13 -0600

    Agregado clase nodo del arbol

---
+++ Lab1_compresion/nodo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_compresion
+{
+    class nodo
+    {
+        nodo izq;
+        nodo der;
+        char caracter;
+
+        public nodo(nodo izq, nodo der, char caracter)
+        {
+            this.izq = izq;
+            this.der = der;
+            this.caracter = caracter;
+        }
+
+        public nodo(nodo izq, nodo der)
+        {
+            this.izq = izq;
+            this.der = der;
+        }
+
+        public char Caracter { get => caracter; set => caracter = value; }
+        internal nodo Izq { get => izq; set => izq = value; }
+        internal nodo Der { get => der; set => der = value; }
+    }
+}
